1f8ab4875dd710a12bc83add8c71f858ed5c6dbf
Camiel Verschoor <verschoor@uva.nl>
2014-04-16 17:29:44 +0200

    eugenio

--- assignment1/assignment1.m
+++ assignment1/assignment1.m
@@ -3,35 +3,63 @@
 %pcl2 = readPcd('data/0000000001.pcd');
 
 % Test data
-pcl2 = [1, 2, 3, 4; 5, 6, 7, 8; 9, 10, 11, 12];
-pcl1 = [1, 1, 1, 1; 2, 2, 2, 2; 3, 3, 3, 3; 4, 4, 4, 4; 5, 5, 5, 5];
+%pcl1 = [1, 2, 3, 4; 5, 6, 7, 8; 9, 10, 11, 12];
+%pcl2 = [1, 1, 1, 1; 2, 2, 2, 2; 3, 3, 3, 3; 4, 4, 4, 4; 5, 5, 5, 5];
+
+pcl1 = [1,1;2,2];
+pcl2 = [1,-1;2, -2]
+
+pcl1rep = repmat(pcl1, [1 1 size(pcl2, 1)]);
+pcl2rep = permute(repmat(pcl2, [1 1 size(pcl1, 1)]), [3 2 1]);
+    
 
 % Data dimension
-d = 4
+d = 4;
 % Rotation Matrix
 R = eye(d);
 % Translation Vector
-
-% We need to minimize the sum of the squares between our initial points and
-% their respective closes points in the second vector. We can only rotate
-% the first points or translate them. Thus:
-%
-% min sum || Ra - t - phi(a) ||
-%
-%
-%
 t = zeros(1,d);
-Mold = ones(4);
-M = eye(4);
-distance = 10;
-while sum(sum(M-Mold)) ~= 0,
-    Mold = M;
-    pcl1rep = repmat(pcl1, [1 1 size(pcl2, 1)]);
+
+counter = 0;
+newdistance = 1;
+while mean(newdistance) > 0.0012 & counter < 20
+    % Compute Euclidean distance
+    distance = sqrt(sum((pcl1rep - pcl2rep).^2, 2));
+    [minima, ids] = min(distance, [], 3);
+
+    % Target cloud
+    target = pcl2(ids, :);
+
+    % Compute centroid
+    centroid1 = sum(pcl1, 1) / size(pcl1, 1);
+    centroid2 = sum(target, 1) / size(target, 1);
+    % Move point clouds toward origin
+    pcl1   = pcl1 - repmat(centroid1, [size(pcl1, 1) 1]);
+    target = target - repmat(centroid2, [size(target, 1) 1]);
+
+    % Compute A matrix
+    A = pcl1' * target;
+
+    % SVD decomposition
+    [U, S, V] = svd(A);
+
+    % Rotation Matrix
+    R = U * V';
+
+    % Translation Matrix
+    T = centroid1 - centroid2 * R;
+
+    % Move Target Cloud
+    pcl2 = R * pcl2' + repmat(T', [1 size(pcl2,1)]);
+    pcl2 = pcl2'
+
+    % Compute new distance
     pcl2rep = permute(repmat(pcl2, [1 1 size(pcl1, 1)]), [3 2 1]);
 
-    distance = sqrt(sum((pcl1rep - pcl2rep).^2, 2));
-    [minimum, id] = min(distance, [], 3)
-
-    M = pcl1\pcl2(id,:)
-    pcl1 = pcl1 * M;
+    newdistance = sqrt(sum((pcl1rep - pcl2rep).^2, 2))
+    counter = counter + 1
 end
+
+%if newdistance < 0.0012
+%    return
+%end
