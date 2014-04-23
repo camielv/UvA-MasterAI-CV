c25ecb2b052c49eaea273945e71eb9642008aee2
Camiel Verschoor <verschoor@uva.nl>
2014-04-23 14:53:44 +0200

    added slow method

---
+++ assignment1/computeForClosestCloud.m
@@ -0,0 +1,11 @@
+function [closestCloud, minima] = computeForClosestCloud(baseCloud, otherCloud)
+baseCloudSize = size(baseCloud, 1);
+for i = 1:baseCloudSize
+    differences = bsxfun(@minus, baseCloud(i), otherCloud);
+    distance = sqrt(sum(differences.^2, 2));
+    [minimum, id] = min(distance);
+    closestCloud(i, :) = otherCloud(id, :);
+    minima(i) = minimum;
+end
+end
+
