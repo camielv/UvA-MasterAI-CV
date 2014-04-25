7b679072a06f963c2971e7cd9f6cde6a0c186659
Camiel Verschoor <verschoor@uva.nl>
2014-04-25 20:01:00 +0200

    Added normal sampling

--- assignment1/src/normalSampling.m
+++ assignment1/src/normalSampling.m
@@ -1,18 +1,51 @@
-function [samples] = normalSampling(cloudName, nSamples, nBins)
-    nBinsX = nBins^(1/3);
+function [sampledIDs] = normalSampling(cloudName, nSamples, nBins)
+    nBinsX = round(nBins^(1/3));
     nBinsY = nBinsX;
-    nBinsZ = nBinsZ;
+    nBinsZ = nBinsX;
+    nBins = nBinsX * nBinsY * nBinsZ;
+
+    dx = 2/nBinsX;
+    dy = 2/nBinsY;
+    dz = 2/nBinsZ;
 
     normals = readNormals(cloudName);
     
-    % TODO: Check if this works correctly
-    normalsIDs = ~any(isnan(normals),2);
-    validNormals = normals(normalIDs);
+    % Remove invalid normals
+    normalIDs = ~any(isnan(normals),2);
+    validNormals = normals(normalIDs, :);
+    normalIDs = find(normalIDs);
 
     % Find bin for each normal.
-    % Find count for each bin
-    % Ascending Sort bins by count
-    % For each bin, extract nSamples / nBins
-    % If you extract less than that, recompute extracting factor by (nSamples - gotSamples)/(nBins - exploredBins)
+    binsX = floor(validNormals(:, 1) / dx) + 1 + ceil(1/dx); binsX(binsX == nBinsX + 1) = nBinsX;
+    binsY = floor(validNormals(:, 2) / dy) + 1 + ceil(1/dy); binsX(binsY == nBinsY + 1) = nBinsY;
+    binsZ = floor(validNormals(:, 3) / dz) + 1 + ceil(1/dz); binsX(binsZ == nBinsZ + 1) = nBinsZ;
 
+    % Find count for each bin
+    bins = [binsX, binsY, binsZ];
+    [uniqueBins, ~, ids] = unique(bins, 'rows'); 
+    counts = accumarray(ids, 1);
+    
+    % Ascending Sort bins by count
+    uniqueBins = sortrows([counts, uniqueBins], 1);
+    nUB = size(uniqueBins, 1);
+
+    sampledIDs = [];
+    gotSamples = 0;
+
+    % For each bin, extract nSamples / nBins
+    for i=1:nUB
+        % If you extract less than that, recompute extracting factor by (nSamples - gotSamples)/(nBins - exploredBins)
+        desiredSamples = ceil((nSamples - gotSamples)/(nUB - i + 1));
+        binsIDs = find(ismember(bins, uniqueBins(i, 2:4), 'rows'));
+        if uniqueBins(i, 1) <= desiredSamples
+            % If less than desired samples, sample everything.
+            gotSamples = gotSamples + uniqueBins(i, 1);
+        else
+            % If more than desired samples, random sample.
+            gotSamples = gotSamples + desiredSamples;
+            binsIDs = randsample(binsIDs, desiredSamples);
+        end
+        realIDs = normalIDs(binsIDs);
+        sampledIDs = [sampledIDs; realIDs];
+    end
 end
