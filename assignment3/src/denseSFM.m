6dacd2a2d0bb6252582c788985048261b269a650
Thomas Jongstra <jongstra@gmail.com>
2014-06-13 17:19:55 +0200

    Fixed denseSMF.m

--- assignment3/src/denseSFM.m
+++ assignment3/src/denseSFM.m
@@ -10,13 +10,12 @@
 function [S,M] = denseSFM(D)
 
 % Apply singular value decomposition on the D matrix.
-[U W Vt] = svd(D);
+[U W V] = svd(D);
 
 % Transpose Vt to V
-V = Vt';
+%V = Vt;
 
 % Truncate matrices
-U
 U3 = U(:,1:3);
 W3 = W(1:3,1:3);
 V3 = V(:,1:3);
@@ -24,8 +23,8 @@ V3 = V(:,1:3);
 % Create motion and shape matrices
 M = U3 * (W3 ^ 0.5);
 S = (W3 ^ 0.5) * V3';
-% M = U3;
-% S = W3 * V3';
+%M = U3;
+%S = W3 * V3';
 
 % Normalize rotation of M
 Ri = [1 0 0] / M(1, :);
